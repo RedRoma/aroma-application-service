73254783cdaf0d71c3a23da085d09adb795aa910
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-24 08:55:39 -0800

    Adding an AuthenticationDecorator

---
+++ src/main/java/tech/aroma/banana/application/service/AuthenticationDecorator.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2015 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.banana.application.service;
+
+import javax.inject.Inject;
+import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.aroma.banana.thrift.application.service.ApplicationService;
+import tech.aroma.banana.thrift.application.service.SendMessageRequest;
+import tech.aroma.banana.thrift.application.service.SendMessageResponse;
+import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
+import tech.aroma.banana.thrift.exceptions.InvalidArgumentException;
+import tech.aroma.banana.thrift.exceptions.InvalidCredentialsException;
+import tech.aroma.banana.thrift.exceptions.InvalidTokenException;
+import tech.aroma.banana.thrift.exceptions.OperationFailedException;
+import tech.sirwellington.alchemy.annotations.access.Internal;
+import tech.sirwellington.alchemy.annotations.designs.patterns.DecoratorPattern;
+
+import static tech.aroma.banana.application.service.ApplicationAssertions.validTokenIn;
+import static tech.sirwellington.alchemy.annotations.designs.patterns.DecoratorPattern.Role.CONCRETE_DECORATOR;
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
+
+/**
+ * Decorates an Application Service, providing Authentication mechanisms against an
+ * {@linkplain AuthenticationService.Iface Authentication Service}.
+ *
+ * @author SirWellington
+ */
+@Internal
+@DecoratorPattern(role = CONCRETE_DECORATOR)
+final class AuthenticationDecorator implements ApplicationService.Iface
+{
+
+    private final static Logger LOG = LoggerFactory.getLogger(AuthenticationDecorator.class);
+
+    private final AuthenticationService.Iface authenticationService;
+    private final ApplicationService.Iface delegate;
+
+    @Inject
+    AuthenticationDecorator(AuthenticationService.Iface authenticationService,
+                            ApplicationService.Iface delegate)
+    {
+        checkThat(delegate, authenticationService)
+            .are(notNull());
+
+        this.authenticationService = authenticationService;
+        this.delegate = delegate;
+    }
+
+    @Override
+    public double getApiVersion() throws TException
+    {
+        return delegate.getApiVersion();
+    }
+
+    @Override
+    public SendMessageResponse sendMessage(SendMessageRequest request) throws OperationFailedException, InvalidArgumentException,
+                                                                              InvalidCredentialsException, TException
+    {
+        checkThat(request)
+            .is(notNull());
+
+        checkThat(request.applicationToken)
+            .throwing(InvalidTokenException.class)
+            .is(validTokenIn(authenticationService));
+
+        return delegate.sendMessage(request);
+    }
+
+    @Override
+    public void sendMessageAsync(SendMessageRequest request) throws TException
+    {
+        checkThat(request)
+            .is(notNull());
+
+        checkThat(request.applicationToken)
+            .throwing(InvalidTokenException.class)
+            .is(validTokenIn(authenticationService));
+
+        delegate.sendMessageAsync(request);
+    }
+
+}
---
+++ src/test/java/tech/aroma/banana/application/service/AuthenticationDecoratorTest.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2015 Aroma Tech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.aroma.banana.application.service;
+
+import org.apache.thrift.TException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import tech.aroma.banana.thrift.application.service.ApplicationService;
+import tech.aroma.banana.thrift.application.service.SendMessageRequest;
+import tech.aroma.banana.thrift.application.service.SendMessageResponse;
+import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
+import tech.aroma.banana.thrift.authentication.service.VerifyTokenRequest;
+import tech.aroma.banana.thrift.exceptions.InvalidTokenException;
+import tech.aroma.banana.thrift.exceptions.OperationFailedException;
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+import tech.sirwellington.alchemy.test.junit.runners.GeneratePojo;
+import tech.sirwellington.alchemy.test.junit.runners.Repeat;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.NumberGenerators.doubles;
+import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Repeat(10)
+@RunWith(AlchemyTestRunner.class)
+public class AuthenticationDecoratorTest
+{
+
+    @Mock
+    private AuthenticationService.Iface authenticationService;
+
+    @Mock
+    private ApplicationService.Iface delegate;
+
+    @GeneratePojo
+    private SendMessageRequest request;
+
+    @GeneratePojo
+    private SendMessageResponse response;
+
+    private AuthenticationDecorator instance;
+
+    @Before
+    public void setUp() throws TException
+    {
+        instance = new AuthenticationDecorator(authenticationService, delegate);
+        verifyZeroInteractions(authenticationService, delegate);
+
+        when(delegate.sendMessage(request))
+            .thenReturn(response);
+    }
+
+    @Test
+    public void testGetApiVersion() throws Exception
+    {
+        double expected = one(doubles(10, 1000));
+        when(delegate.getApiVersion())
+            .thenReturn(expected);
+
+        double result = instance.getApiVersion();
+        assertThat(result, is(expected));
+        verify(delegate).getApiVersion();
+
+    }
+
+    @Test
+    public void testGetApiVersionWhenFails() throws Exception
+    {
+        when(delegate.getApiVersion())
+            .thenThrow(new OperationFailedException());
+
+        assertThrows(() -> instance.getApiVersion())
+            .isInstanceOf(OperationFailedException.class);
+
+    }
+
+    @Test
+    public void testSendMessage() throws Exception
+    {
+        SendMessageResponse result = instance.sendMessage(request);
+
+        assertThat(result, is(response));
+        verify(delegate).sendMessage(request);
+
+        VerifyTokenRequest expectedRequest = new VerifyTokenRequest()
+            .setTokenId(request.applicationToken.tokenId);
+
+        verify(authenticationService).verifyToken(expectedRequest);
+    }
+
+    @Test
+    public void testSendMessageWithInvalidToken() throws Exception
+    {
+        when(authenticationService.verifyToken(Mockito.any()))
+            .thenThrow(new InvalidTokenException());
+
+        assertThrows(() -> instance.sendMessage(request))
+            .isInstanceOf(InvalidTokenException.class);
+
+        verifyZeroInteractions(delegate);
+    }
+
+    @Test
+    public void testSendMessageAsync() throws Exception
+    {
+        instance.sendMessageAsync(request);
+
+        verify(delegate).sendMessageAsync(request);
+
+        VerifyTokenRequest expectedRequest = new VerifyTokenRequest()
+            .setTokenId(request.applicationToken.tokenId);
+
+        verify(authenticationService).verifyToken(expectedRequest);
+
+    }
+
+    @Test
+    public void testSendMessageAsyncWithInvalidToken() throws Exception
+    {
+        when(authenticationService.verifyToken(Mockito.any()))
+            .thenThrow(new InvalidTokenException());
+
+        assertThrows(() -> instance.sendMessageAsync(request))
+            .isInstanceOf(InvalidTokenException.class);
+
+        verifyZeroInteractions(delegate);
+    }
+
+}
