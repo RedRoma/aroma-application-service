746abefc4fdfb6bb3c164257eba0d570be10a7b5
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-22 09:29:34 -0800

    ApplicationServiceModuleTest: Adding Cassandra Data Modules support

--- src/test/java/tech/aroma/banana/application/service/ApplicationServiceModuleTest.java
+++ src/test/java/tech/aroma/banana/application/service/ApplicationServiceModuleTest.java
@@ -26,17 +26,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import tech.aroma.banana.application.service.operations.ApplicationServiceOperationsModule;
+import tech.aroma.banana.application.service.operations.ModuleApplicationServiceOperations;
+import tech.aroma.banana.data.cassandra.ModuleCassandraDataRepositories;
+import tech.aroma.banana.data.cassandra.ModuleCassandraDevCluster;
 import tech.aroma.banana.thrift.application.service.ApplicationService;
 import tech.aroma.banana.thrift.application.service.SendMessageRequest;
 import tech.aroma.banana.thrift.application.service.SendMessageResponse;
 import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
+import tech.aroma.banana.thrift.notification.service.NotificationService;
 import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
 import tech.sirwellington.alchemy.test.junit.runners.GeneratePojo;
 import tech.sirwellington.alchemy.thrift.operations.ThriftOperation;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -51,12 +55,17 @@ public class ApplicationServiceModuleTest
     private AuthenticationService.Iface authenticationService;
 
     @Mock
+    private NotificationService.Iface notificationService;
+
+    @Mock
     private ThriftOperation<SendMessageRequest, SendMessageResponse> sendMessageOperation;
 
     private ApplicationServiceModule module;
-    private FakeModule fakeModule;
-    private ApplicationServiceOperationsModule operationModule;
-    
+    private ModuleMocks moduleFake;
+    private ModuleApplicationServiceOperations moduleOperation;
+    private ModuleCassandraDataRepositories moduleCassandraData;
+    private ModuleCassandraDevCluster moduleCassandraDevCluster;
+
     @GeneratePojo
     private SendMessageRequest request;
 
@@ -64,35 +73,48 @@ public class ApplicationServiceModuleTest
     public void setUp()
     {
         module = new ApplicationServiceModule();
-        fakeModule = new FakeModule(authenticationService, sendMessageOperation);
-        operationModule = new ApplicationServiceOperationsModule();
+        moduleFake = new ModuleMocks(authenticationService, sendMessageOperation);
+        moduleOperation = new ModuleApplicationServiceOperations();
+        moduleCassandraData = new ModuleCassandraDataRepositories();
+        moduleCassandraDevCluster = new ModuleCassandraDevCluster();
     }
 
     @Test
     public void testConfigure() throws TException
     {
-        Injector injector = Guice.createInjector(fakeModule, module);
+        Injector injector = Guice.createInjector(moduleFake, module);
+
         ApplicationService.Iface service = injector.getInstance(ApplicationService.Iface.class);
         assertThat(service, notNullValue());
-        
+
+        ensureAuthenticationServiceIsCalled(service);
+    }
+
+    private void ensureAuthenticationServiceIsCalled(ApplicationService.Iface service) throws TException
+    {
         service.sendMessage(request);
         verify(authenticationService).verifyToken(Mockito.any());
     }
-    
+
     @Test
     public void testConfigureWithRealModules() throws Exception
     {
-        Injector injector = Guice.createInjector(operationModule, module, new AuthenticationModule());
+        Injector injector = Guice.createInjector(moduleCassandraData,
+                                                 moduleCassandraDevCluster,
+                                                 moduleOperation,
+                                                 module,
+                                                 new ModuleAuthentication(),
+                                                 new ModuleNotification());
         assertThat(injector, notNullValue());
     }
 
-    private static class FakeModule extends AbstractModule
+    private static class ModuleMocks extends AbstractModule
     {
 
         private final AuthenticationService.Iface authenticationService;
         private final ThriftOperation<SendMessageRequest, SendMessageResponse> sendMessageOperation;
 
-        FakeModule(AuthenticationService.Iface authenticationService,
+        ModuleMocks(AuthenticationService.Iface authenticationService,
                    ThriftOperation<SendMessageRequest, SendMessageResponse> sendMessageOperation)
         {
             this.authenticationService = authenticationService;
@@ -109,8 +131,8 @@ public class ApplicationServiceModuleTest
         }
 
     }
-    
-    private static class AuthenticationModule extends AbstractModule
+
+    private static class ModuleAuthentication extends AbstractModule
     {
 
         @Override
@@ -118,8 +140,23 @@ public class ApplicationServiceModuleTest
         {
             AuthenticationService.Iface authenticationService;
             authenticationService = Mockito.mock(AuthenticationService.Iface.class);
-            
+
             bind(AuthenticationService.Iface.class).toInstance(authenticationService);
+        }
+
+    }
+    
+    private static class ModuleNotification extends AbstractModule
+    {
+
+        @Override
+        protected void configure()
+        {
+            NotificationService.Iface notificationService;
+            notificationService = mock(NotificationService.Iface.class);
+            
+            bind(NotificationService.Iface.class)
+                .toInstance(notificationService);
         }
         
     }
