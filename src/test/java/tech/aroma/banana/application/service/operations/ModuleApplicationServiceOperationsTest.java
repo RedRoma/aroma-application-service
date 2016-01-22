a8d52be7812bd30240ca77e7ba6de9276ba0c48a
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-01-22 10:31:57 -0800

    ModuleApplicationServiceOperationsTest: Adding mocks of missing bindings

--- src/test/java/tech/aroma/banana/application/service/operations/ModuleApplicationServiceOperationsTest.java
+++ src/test/java/tech/aroma/banana/application/service/operations/ModuleApplicationServiceOperationsTest.java
@@ -16,38 +16,72 @@
 
 package tech.aroma.banana.application.service.operations;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Provides;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import tech.aroma.banana.data.memory.ModuleMemoryDataRepositories;
+import tech.aroma.banana.thrift.authentication.service.AuthenticationService;
+import tech.aroma.banana.thrift.notification.service.NotificationService;
+import tech.sirwellington.alchemy.annotations.testing.IntegrationTest;
 import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 
 /**
  *
  * @author SirWellington
  */
+@IntegrationTest
 @RunWith(AlchemyTestRunner.class)
-public class ModuleApplicationServiceOperationsTest 
+public class ModuleApplicationServiceOperationsTest
 {
+
+    private ModuleMocks moduleMocks;
+    private ModuleMemoryDataRepositories moduleData;
     private ModuleApplicationServiceOperations module;
-    
-    
+
     @Before
     public void setUp()
     {
+        moduleMocks = new ModuleMocks();
+        moduleData = new ModuleMemoryDataRepositories();
         module = new ModuleApplicationServiceOperations();
     }
 
     @Test
     public void testConfigure()
     {
-        Injector injector = Guice.createInjector(module);
-        
+        Injector injector = Guice.createInjector(module, moduleMocks, moduleData);
+
         assertThat(injector, notNullValue());
+    }
+
+    private static class ModuleMocks extends AbstractModule
+    {
+
+        @Override
+        protected void configure()
+        {
+
+        }
+
+        @Provides
+        AuthenticationService.Iface provideMockAuthenticationService()
+        {
+            return mock(AuthenticationService.Iface.class);
+        }
+
+        @Provides
+        NotificationService.Iface provideMockNotificationService()
+        {
+            return mock(NotificationService.Iface.class);
+        }
     }
 
 }
