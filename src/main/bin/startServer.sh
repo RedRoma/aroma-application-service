bb4b3ffd6d11a9a208cdbf9bc543d6a67f6d5b9a
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-02-22 12:44:50 -0800

    Updating startServer script

--- src/main/bin/startServer.sh
+++ src/main/bin/startServer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version="1.0-SNAPSHOT"
-jar="banana-application-service-$version.jar"
+version="1.1-SNAPSHOT"
+jar="aroma-application-service-$version.jar"
 
 nohup java -jar $jar > server.log &
