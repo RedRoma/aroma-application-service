8bac1d45649eda841ca18d73c84294d2b862e5f3
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-04-25 00:20:08 -0700

    MatchAlgorithm updates

--- src/main/java/tech/aroma/application/service/reactions/matchers/MatchAlgorithm.java
+++ src/main/java/tech/aroma/application/service/reactions/matchers/MatchAlgorithm.java
@@ -16,8 +16,10 @@
 
 package tech.aroma.application.service.reactions.matchers;
 
+import com.google.inject.ImplementedBy;
 import java.util.List;
 import java.util.Objects;
+import javax.inject.Inject;
 import sir.wellington.alchemy.collections.lists.Lists;
 import tech.aroma.thrift.Message;
 import tech.aroma.thrift.reactions.AromaMatcher;
@@ -26,24 +28,39 @@ import tech.sirwellington.alchemy.annotations.arguments.Required;
 import tech.sirwellington.alchemy.annotations.designs.patterns.StrategyPattern;
 
 import static java.util.stream.Collectors.toList;
+import static tech.sirwellington.alchemy.annotations.designs.patterns.StrategyPattern.Role.CONCRETE_BEHAVIOR;
 import static tech.sirwellington.alchemy.annotations.designs.patterns.StrategyPattern.Role.INTERFACE;
+import static tech.sirwellington.alchemy.arguments.Arguments.checkThat;
+import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
 
 /**
- *
+ * The matching algorithm decides on what criteria to judge whether a Message Matches a set
+ * of {@linkplain AromaMatcher Matchers}. For instance, an <b>AND</b> operation vs an <b>OR</b> operation.
+ * 
  * @author SirWellington
  */
 @StrategyPattern(role = INTERFACE)
+@ImplementedBy(MatchAlgorithm.AndImpl.class)
 public interface MatchAlgorithm
 {
-
+    
     boolean matches(@Required Message message, @NonEmpty List<AromaMatcher> matchers);
     
-
+    @StrategyPattern(role = CONCRETE_BEHAVIOR)
     static class AndImpl implements MatchAlgorithm
     {
-
-        private MatcherFactory matcherFactory;
-
+        
+        private final MatcherFactory matcherFactory;
+        
+        @Inject
+        AndImpl(MatcherFactory matcherFactory)
+        {
+            checkThat(matcherFactory)
+                .is(notNull());
+            
+            this.matcherFactory = matcherFactory;
+        }
+        
         @Override
         public boolean matches(Message message, List<AromaMatcher> matchers)
         {
@@ -51,27 +68,35 @@ public interface MatchAlgorithm
             {
                 return false;
             }
-
+            
             List<MessageMatcher> reactionMatchers = matchers
                 .stream()
                 .map(matcherFactory::matcherFor)
                 .filter(Objects::nonNull)
                 .collect(toList());
-
+            
             long totalMatches = reactionMatchers.stream()
                 .filter(matcher -> matcher.matches(message))
                 .count();
-
+            
             return totalMatches == matchers.size();
         }
-
+        
     }
-
+    
+    @StrategyPattern(role = CONCRETE_BEHAVIOR)
     static class OrImpl implements MatchAlgorithm
     {
-
-        private MatcherFactory factory;
-
+        
+        private final MatcherFactory factory;
+        
+        @Inject
+        OrImpl(MatcherFactory factory)
+        {
+            checkThat(factory).is(notNull());
+            this.factory = factory;
+        }
+        
         @Override
         public boolean matches(Message message, List<AromaMatcher> matchers)
         {
@@ -79,15 +104,15 @@ public interface MatchAlgorithm
             {
                 return false;
             }
-
+            
             long totalMatches = matchers.stream()
                 .map(factory::matcherFor)
                 .filter(Objects::nonNull)
                 .filter(matcher -> matcher.matches(message))
                 .count();
-
+            
             return totalMatches > 0;
         }
     }
-
+    
 }
