4ff1cea35ac533d3cb533af98f45494b9f86500d
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-04-25 12:02:40 -0700

    Unit test addition: MessageReactorImplTest

--- src/test/java/tech/aroma/application/service/reactions/MessageReactorImplTest.java
+++ src/test/java/tech/aroma/application/service/reactions/MessageReactorImplTest.java
@@ -64,7 +64,7 @@ import static tech.sirwellington.alchemy.test.junit.runners.GenerateString.Type.
  *
  * @author SirWellington
  */
-@Repeat(10)
+@Repeat(50)
 @RunWith(AlchemyTestRunner.class)
 public class MessageReactorImplTest
 {
